e7d6991d1c22cf0e6c4754f3415f43c23da395b8
Nilamadhabp <nilamadhabp@192.168.1.2>
2026-03-17 22:51:59 +0530

    fixed lint issues

--- app.py
+++ app.py
@@ -1,59 +1,58 @@
-from flask import Flask,render_template,jsonify
+from flask import Flask, render_template, jsonify
 import os
 import socket
 import datetime
-import subprocess
+
 
 app = Flask(__name__)
 
-VERSION=os.getenv("GITHUB_SHA","local")
-IMAGE=os.getenv("IMAGE","github-actions-capstone")
+VERSION = os.getenv("GITHUB_SHA", "local")
+IMAGE = os.getenv("IMAGE", "github-actions-capstone")
+
 
 def check_service(name):
-
     try:
         return "Healthy"
-    except:
+    except Exception:
         return "Down"
+
 
 @app.route("/")
 def home():
-
     return render_template("dashboard.html")
 
-@app.route("/health")
 
+@app.route("/health")
 def health():
 
-    return jsonify({
+    return jsonify(
+        {
+            "status": "healthy",
+            "time": str(datetime.datetime.now()),
+            "host": socket.gethostname(),
+        }
+    )
 
-        "status":"healthy",
-        "time":str(datetime.datetime.now()),
-        "host":socket.gethostname()
-
-    })
 
 @app.route("/metrics")
-
 def metrics():
 
-    data={
-
-        "service":"Payment API",
-        "version":VERSION[:7],
-        "image":IMAGE,
-        "environment":"production",
-        "pipeline":"Passing",
-        "security":"Scanned",
-        "time":str(datetime.datetime.now()),
-        "host":socket.gethostname(),
-        "python":os.sys.version.split()[0]
-
+    data = {
+        "service": "Payment API",
+        "version": VERSION[:7],
+        "image": IMAGE,
+        "environment": "production",
+        "pipeline": "Passing",
+        "security": "Scanned",
+        "time": str(datetime.datetime.now()),
+        "host": socket.gethostname(),
+        "python": os.sys.version.split()[0],
     }
 
     return jsonify(data)
 
+
 if __name__ == "__main__":
 
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0", port=5000)
     
